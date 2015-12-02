34b9c662858fe19765b80dfb778f5096c6bb8098
Sam Morgan <s_morgan@me.com>
2015-12-02 16:51:38 +0000

    refactor step

--- spec/features/skill_check_spec.rb
+++ spec/features/skill_check_spec.rb
@@ -6,5 +6,6 @@ describe 'Making a skill check' do
     character = Character.new
     stub_roll(15)
     expect(SkillCheck.with(character, :jump, 15)).to eq true
+    expect(SkillCheck.with(character, :jump, 20)).to eq false
   end
 end
--- spec/services/skill_check_spec.rb
+++ spec/services/skill_check_spec.rb
@@ -4,28 +4,16 @@ describe SkillCheck do
   let(:character) { double :character, skills: 0 }
 
   describe '.with' do
-    it 'responds' do
-      expect(described_class).to respond_to(:with).with(3).arguments
+    before do
+      stub_roll(15)
     end
 
-    context 'when the roll is high' do
-      before do
-        stub_roll(15)
-      end
-
-      it 'makes a successful check' do
-        expect(described_class.with(character, :jump, 15)).to eq true
-      end
+    it 'can make a successful check' do
+      expect(described_class.with(character, :jump, 15)).to eq true
     end
 
-    context 'when the roll is low' do
-      before do
-        stub_roll(5)
-      end
-
-      it 'makes a failed check' do
-        expect(described_class.with(character, :jump, 15)).to eq false
-      end
+    it 'can make a failed check' do
+      expect(described_class.with(character, :jump, 20)).to eq false
     end
   end
 end
